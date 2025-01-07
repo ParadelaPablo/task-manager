6e3433cf72bb8ad4080685a0075b4d5c441e726e
Pablo Paradela <pablo.paradela@appliedtechnology.se>
2025-01-07 20:50:03 +0100

    Update axios baseURL to use environment variable VITE_API_URL

--- frontend/src/services/axiosInstance.tsx
+++ frontend/src/services/axiosInstance.tsx
@@ -1,12 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080/api",
     headers: {
         "Content-Type": "application/json",
     },
 });
-
 
 axiosInstance.interceptors.request.use((request) => {
     console.log("Starting Request", request);
